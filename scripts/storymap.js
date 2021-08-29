e4d48797333319fd14af5b3d5187ae57a6e905ff
taralyh <89719122+taralyh@users.noreply.github.com>
2021-08-29 13:38:19 -0700

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -152,7 +152,7 @@ $(window).on('load', function() {
         markers.push(
           L.marker([lat, lon], {
             icon: L.ExtraMarkers.icon({
-              icon: 'fa-number',
+              icon: c['Marker Icon'],
               number: c['Marker'] === 'Plain' ? '' : chapterCount,
               markerColor: c['Marker Color'] || 'blue'
             }),
