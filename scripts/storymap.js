2784a76c028079e41024b24f3c36359754892cbb
taralyh <89719122+taralyh@users.noreply.github.com>
2021-09-02 10:28:28 -0700

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -220,7 +220,7 @@ $(window).on('load', function() {
         });
         
         mediaContainer = $('<div></div>', {
-          class: 'audio-container'
+          class: 'img-container'
         }).append(media).after(source);
       }
         
